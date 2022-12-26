ade9e2c60fb191b17903b2446cf2bed522b84d6a
Rasujon3 <rasujon3@gmail.com>
2022-12-26 22:26:23 +0600

    first commit

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  email: "",
+  role: "",
+  isLoading: true,
+  isError: false,
+  error: "",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+export const {} = authSlice.actions;
+export default authSlice.reducer;
