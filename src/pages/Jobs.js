4f2bae67f1d5e8e85dd54cfd381ccf8933dfbad0
Rasujon3 <rasujon3@gmail.com>
2023-01-04 00:42:04 +0600

    01 Job page overview

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import JobCard from "../components/reusable/JobCard";
 import { useGetJobsQuery } from "../features/job/jobApi";
 
 const Jobs = () => {
@@ -10,20 +11,14 @@ const Jobs = () => {
 
   return (
     <div className="pt-14">
-      <h1>This is job page</h1>
-      <div>
-        {data?.data?.map(({ position, companyName, _id }) => (
-          <div>
-            <h1>{position}</h1>
-            <p>{companyName}</p>
-            <button
-              className="border"
-              onClick={() => navigate(`/job-details/${_id}`)}
-            >
-              Details
-            </button>
-          </div>
+      <div className="bg-primary/10 p-5 rounded-2xl">
+        <h1 className="font-semibold text-xl">Find Jobs</h1>
+      </div>
+      <div className="grid grid-cols-2 gap-5 mt-5">
+        {data?.data?.map((jobData) => (
+          <JobCard key={jobData._id} jobData={jobData} />
         ))}
+        {/* <JobCard /> */}
       </div>
     </div>
   );
